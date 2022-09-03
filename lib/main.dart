cb0d7ead53796dab8d6715a2319722dba580a81f
ibrahimahmedkhan <ibrahimahmed.ayesha@gmail.com>
2022-09-03 17:44:43 +0500

    Added elevation to list tiles

--- lib/main.dart
+++ lib/main.dart
@@ -39,14 +39,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   List<User> users = [
     User(name: "Google", email: "gmaild@gmail.com", color: Colors.green),
     User(name: "Facebook", email: "facebook@gmail.com", color: Colors.red),
@@ -92,11 +84,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   )
                   .toList())),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
